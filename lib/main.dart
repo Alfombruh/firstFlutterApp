27ce6f280d3dfca4198f293446165b652a238983
Alfombruh <jokinfernandez10@gmail.com>
2023-10-31 21:49:05 +0100

    ionmi se come los mocos pero no se lo dice a nadie para quue no se los roben

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -49,52 +47,141 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+// ignore: must_be_immutable
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedPage) {
+      case 0:
+        page = NameGeneratorPage();
+        break;
+      case 1:
+        page = NameList();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedPage');
+    }
+    return Scaffold(
+        backgroundColor: Theme.of(context).secondaryHeaderColor,
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: true,
+                selectedIndex: selectedPage,
+                destinations: [
+                  NavigationRailDestination(
+                      icon: Icon(Icons.home), label: Text("Homepage")),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.favorite), label: Text("Favourites"))
+                ],
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedPage = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+                child: Container(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    child: page))
+          ],
+        ));
+  }
+}
+
+class NameList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favourites.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('No favourites Yet')],
+        ),
+      );
+    }
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text("YOU HAVE " "${appState.favourites.length} favourites:"),
+        ),
+        for (var pair in appState.favourites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asPascalCase),
+          )
+      ],
+    );
+  }
+}
+
+class NameGeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
+    Icon icon;
+    if (appState.favourites.contains(appState.current)) {
+      icon = Icon(
+        Icons.favorite,
+        color: Colors.white,
+        size: 15.0,
+      );
+    } else {
+      icon = Icon(
+        Icons.favorite_border,
+        color: Colors.white,
+        size: 15.0,
+      );
+    }
 
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                MyButton("Next Name", appState.getNext, Icon(Icons.sailing)),
-                SizedBox(
-                  width: 5,
-                ),
-                MyButton("Favourites", appState.addFavourites, Icon(
-                Icons.favorite,
-                color: Colors.white,
-                size: 15.0,
-                semanticLabel: 'You liked this',
-                )
-                ),
-              ],
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              MyButton(
+                  "Next Name",
+                  appState.getNext,
+                  Icon(
+                    Icons.sailing,
+                    color: Colors.white,
+                    size: 15.0,
+                  )),
+              SizedBox(
+                width: 5,
+              ),
+              MyButton("Favourites", appState.addFavourites, icon),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
 
 class MyButton extends StatelessWidget {
-  const MyButton(
-    this.text,
-    this.callback,
-    [this.icon]
-  );
+  const MyButton(this.text, this.callback, [this.icon]);
   final VoidCallback callback;
   final String text;
   final Icon? icon;
@@ -102,15 +189,15 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton.icon(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.pink[400],
-        ),
-        onPressed: () {
-          callback();
-        },
-        label: ButtonText(text),
-        icon: icon?? SizedBox.shrink(),
-        );
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.pink[400],
+      ),
+      onPressed: () {
+        callback();
+      },
+      label: ButtonText(text),
+      icon: icon ?? SizedBox.shrink(),
+    );
   }
 }
 
